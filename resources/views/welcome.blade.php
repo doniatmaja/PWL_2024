a8fdc65c2b3db8d19c92cdadc4812225c862126b
donikus <kusumaodon47@gmail.com>
2025-04-23 13:01:09 +0700

    perubahan web besar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getlocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PWL 2024</title>
-    </head>
-    <body>
-        <h2>Pemrograman Web Lanjut 2024</h2>
-        <h3>Semangat Belajar Laravel 10</h3>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>'Nama : Doni kusuma Atmaja'</h1>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,27 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
 Route::get('/user/{name?}', function ($name= 'Doni') {
     return 'Nama saya  ' . $name;
 });
+
+use App\Http\Controllers\WelcomeController;
+
+// Route untuk halaman utama
+Route::get('/index', [WelcomeController::class, 'index']);
+
+// Route untuk halaman utama
+Route::get('/about', [WelcomeController::class, 'about']);
+
+// Route untuk halaman utama
+Route::get('/articles/{id}', [WelcomeController::class, 'articles']);
+
+use App\Http\Controllers\PhotoController;
+
+// Route untuk halaman utama
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
